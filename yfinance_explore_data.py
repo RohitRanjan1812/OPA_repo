bce4215c650635a2c139ce700e7c08895d66c78c
Rohit Ranjan <rohitranjan1812@gmail.com>
2022-12-02 13:08:19 +0530

    another commit

--- yfinance_explore_data.py
+++ yfinance_explore_data.py
@@ -1,2 +1,18 @@
+import pandas as pd
 import yfinance as yf
-print(yf.Ticker('DIA').info)
+df = pd.read_excel("tickers.ods", engine="odf")
+# print(df.head())
+tickers = list(df.Tickers)
+
+# too big df due to changing columns
+# df_allInfo = pd.DataFrame([])
+# for t in tickers:
+#     temp_df = pd.DataFrame.from_dict([yf.Ticker(t).info]) 
+#     df_allInfo = pd.concat([df_allInfo, temp_df])
+
+# print(df_allInfo.head())
+
+#find all distinct columns
+cols = set()
+for t in tickers[:100]:
+    cols = cols.union(set(yf.Ticker(t).info.keys()))
