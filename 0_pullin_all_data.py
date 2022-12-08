e65927ce3fb4cee34b4ffb79fab08dd2c07eaaa1
Rohit Ranjan <rohitranjan1812@gmail.com>
2022-12-08 07:13:39 +0530

    steps needed to create a full info extract on all our Tickers (410)

---
+++ 0_pullin_all_data.py
@@ -0,0 +1,46 @@
+#%%
+import pandas as pd
+import os
+import yfinance as yf
+
+directory = r'C:\Users\49176\Desktop\DSA\OPA_repo\Data\csv'
+sp_data_ticker = list()
+for f in os.listdir(directory):
+    f_path = os.path.join(directory, f)
+    if os.path.isfile(f_path):
+        sp_data_ticker.append(f[:-4])
+        #print(f_path)
+        #print(sp_data_ticker)
+yf_ticker_df = pd.read_excel(r'C:\Users\49176\Desktop\DSA\OPA_repo\Data\tickers_usa.ods', engine='odf')
+yf_ticker = list(yf_ticker_df.Ticker)
+
+print('check if all data tickers are in yfinace ticker list:', set(sp_data_ticker) - set(yf_ticker) == set())
+
+#%%
+df_allInfo = pd.DataFrame([])
+for t in sp_data_ticker:
+    temp_df = pd.DataFrame.from_dict([yf.Ticker(t).info]) 
+    df_allInfo = pd.concat([df_allInfo, temp_df])
+
+df_allInfo.to_pickle(r'C:\Users\49176\Desktop\DSA\OPA_repo\df_allInfo_124.pkl')
+
+#%%
+#for handling incompatible inputs
+issue_tickers = []
+for t in list(set(sp_data_ticker) - set(df_allInfo.symbol)):
+    try:
+        temp_df = pd.DataFrame.from_dict([yf.Ticker(t).info]) 
+        df_allInfo = pd.concat([df_allInfo, temp_df])
+    except:
+        issue_tickers.append(t)
+        continue
+
+#['REGN', 'TJX', 'TRAUF'] are the issue tickers -> we will manually append them
+df_allInfo = pd.concat([df_allInfo, pd.DataFrame.from_dict([yf.Ticker('REGN').info])])
+df_allInfo = pd.concat([df_allInfo, pd.DataFrame.from_dict([yf.Ticker('TJX').info])])
+df_allInfo = pd.concat([df_allInfo, pd.DataFrame.from_dict([yf.Ticker('TRAUF').info])])
+#the manual append works fine, api call times could have been an issue why it threw an exception
+
+#%% finally we can save our complete extract as pkl
+df_allInfo.to_pickle(r'C:\Users\49176\Desktop\DSA\OPA_repo\df_allInfo.pkl')
+
